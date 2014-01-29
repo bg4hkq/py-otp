18f29ace717992e887b1d7e51d8878e23750719d
Ayrx <terrycwk1994@gmail.com>
2014-01-29 18:13:44 +0800

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+__author__ = 'Terry Chia'
+
+setup(
+    name='py-otp',
+    version='1.0',
+    description='Google Authenticator compatible HOTP & TOTP Library',
+    author='Terry Chia',
+    author_email='terrycwk1994@gmail.com',
+    url='https://github.com/Ayrx/py-otp',
+    packages=['otp'],
+    license='MIT'
+)
